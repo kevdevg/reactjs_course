034e258abd22a7e836c7222861cb7dca1f0b93e7
kevdevg <34045043+kevdevg@users.noreply.github.com>
2017-11-29 15:08:54 -0500

    corrección immutable

--- class1_inmutable.js
+++ class1_inmutable.js
@@ -1,18 +1,18 @@
-const { List } = require('immutable')
+const { List, Map } = require('immutable')
 const a = List([1, 3, 4, 6, 12, 15, 23, 20 ]);
 const b = List(['a', 'once a time', 'machine learning', 'beer', 'bear', 'beat']);
-const c = List([ { key1: 'abc' }, { key2: 1.2 }, { key3: false }, { key4: null }, { key5: undefined }, { key6: '' }]);
+const c = List([ Map({ key1: 'abc' }), Map({ key2: 1.2 }), Map({ key3: false }), Map({ key4: null }), Map({ key5: undefined }), Map({ key6: '' })]);
 //a 
 const odd_numbers = a.filter(n => n % 2 == 0);
-console.log("a", odd_numbers)
+console.log("a", odd_numbers);
 //b
 const double_numbers = a.map(n => n * 2);
-console.log("b", double_numbers)
+console.log("b", double_numbers);
 //c
 const be_includes_values = b.filter(n => n.includes("be"));
-console.log("c", be_includes_values)
+console.log("c", be_includes_values);
 // d
 const false_values = c.filter(v => {
-    return v[Object.keys(v)[0]] == false;
+    return v.first() == false;
 });
-console.log("c", false_values)
+console.log("c", false_values);

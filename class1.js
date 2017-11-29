883325d51e08ca31f14be5e736e62322051bd784
kevdevg <34045043+kevdevg@users.noreply.github.com>
2017-11-29 11:10:20 -0500

    logs

--- class1.js
+++ class1.js
@@ -4,12 +4,16 @@ const a = [1, 3, 4, 6, 12, 15, 23, 20];
 const b = ['a', 'once a time', 'machine learning', 'beer', 'bear', 'beat']
 const c = [ { key1: 'abc' }, { key2: 1.2 }, { key3: false }, { key4: null }, { key5: undefined }, { key6: '' }];
 // A
-const odd_numbers = fp.filter(n => n % 2 == 0)(a)
+const odd_numbers = fp.filter(n => n % 2 == 0)(a);
+console.log("a", odd_numbers);
 // B
-const double_values = fp.map(n => n * 2)(a)
+const double_values = fp.map(n => n * 2)(a);
+console.log("b", double_values);
 // C
-const be_inclues_values = fp.filter(n => n.includes('be'))(b)
+const be_inclues_values = fp.filter(n => n.includes('be'))(b);
+console.log("c", be_inclues_values);
 // D
 const false_values = fp.filter(v => {
     return v[Object.keys(v)[0]] == false;
-})(c)
+})(c);
+console.log("d", false_values);
